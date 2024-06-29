26ad0479bf4db4165d5ae281113d9e18969c771e
Brando Elí Carrillo Pérez <belicarrillo30@gmail.com>
2024-06-29 11:30:36 -0500

    Comentario hecho

--- src/main/java/com/configserver/ConfigServerApplication.java
+++ src/main/java/com/configserver/ConfigServerApplication.java
@@ -3,7 +3,7 @@ package com.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+// Este servidor es de utilería que depende de un repositorio remoto con propiedades comunes de comunicación para los microservicios a través de Eureka
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
